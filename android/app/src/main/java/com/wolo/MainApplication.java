7a9db23cf0279f11918699ff1d00f55c0861baa9
Andreas Reiterer <reiterer.a@gmail.com>
2017-01-18 17:29:33 +0100

    Add support for Icon.ToolbarAndroid

--- android/app/src/main/java/com/wolo/MainApplication.java
+++ android/app/src/main/java/com/wolo/MainApplication.java
@@ -13,6 +13,8 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
+import com.oblador.vectoricons.VectorIconsPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -25,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage()
+          , new VectorIconsPackage()
       );
     }
   };
